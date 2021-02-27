ddd58c1657aa6ff255aa94302ce8733b46756e0f
unknown <knightkrypto7@gmail.com>
2021-02-27 17:01:06 +0800

    fixed widht of crypto-wrapper class

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,17 @@
 
   * {
     margin: 0;
-    padding: 0 !important;
+    padding: 0;
     box-sizing: border-box;
   }
   
  .content {
+   position: absolute;
    display: grid;
-   grid-template-columns: 10px 1fr 10px;
+   grid-template-columns: 0.5fr 1fr 0.5fr;
    grid-template-rows: 70px 1fr;
-   grid-gap: 30px;
-   height: 100vh;
-   width: 100vw;
+   grid-gap: 20px;
+   width: 100%;
  }
  
  .content .cryptos {
@@ -26,95 +26,41 @@
    grid-column: 2/3;
    grid-row: 2/4;
    width: 100%;
+   max-width: 1000px;
  } 
  
   .content > h1 {
     justify-self: center;
     align-self: center;
-    font-size: 1.5rem;
+    font-size: 1.4rem;
     grid-column: 2/3;
     grid-row: 1/2;
-  }
-  
-  .crypto-wrapper {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    border: 1px solid red;
-    margin: 20px;
-    height: 180px;
-    width: 100%;
-    background: #2b3389;
-    color: white;
-  }
-  
-  .crypto-wrapper > h1 > img {
-    position: relative;
-    height: 50px;
-    width: 50px;
-  }
-  
-  .crypto-wrapper .crypto-prices {
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
-    width: 80%;
-    height: 80px;
-  }
-  
-  .crypto-prices .buy {
-    border-radius: 6px;
-    background: green;
-  }
-  
-  .crypto-prices .sell {
-    border-radius: 6px;
-    background: red;
   }
   
 </style>
 
 <script>
-
   import {onMount} from "svelte"
+  import Crypto from "./Crypto.svelte"
   import axios from "axios"
-  
-  /*
-  let dummyData = [
-    {name: "BTC", buy: 46400, sell: 44600, currency: {name: "USD", symbol: "$"}}, 
-    {name: "ETH", buy: 46400, sell: 44600, currency: {name: "USD", symbol: "$"}}, 
-    {name: "BCH", buy: 46400, sell: 44600, currency: {name: "USD", symbol: "$"}}
- ]
- */
- 
- $: data = ""
- 
-onMount(async function(){
-   const res = await axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
- /* .then(res => {
-    data = res.data
-  })
-  alert(await res.data)*/
-  data = await res.data
-})
-  
-</script>
 
-  <div class="content">
+  $: data = ""
+  $: currency = "usd"
+ 
+  onMount(async function(){
+    const res = await axios(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}`)
+    data = await res.data
+  })
+</script>
+<div class="content">
   <h1>Cryptocurrency</h1>
+  <select bind:value="{currency}">
+    <option value="usd">USD</option>
+    <option value="cad">CAD</option>
+  </select>
   <div class="cryptos">
       {#each data as crypto} 
-    <div class="crypto-wrapper">
-        <h1>{crypto.name} <img src={crypto.image} alt={crypto.name} /></h1>
-        <div class="crypto-prices">
-          <div class="buy"> $ {crypto.current_price} </div>
-          <div class="sell">$ {crypto.low_24h}</div>
-        </div>
-    </div>
+        <Crypto {crypto} />
       {/each}
   </div>
-  </div>
+</div>
---
+++ src/Crypto.svelte
@@ -0,0 +1,69 @@
+<script>
+   export let crypto
+   
+</script>
+
+<div class="crypto-wrapper">
+    <h1>{crypto.name} <img src={crypto.image} alt={crypto.name} /></h1>
+    <div class="crypto-prices">
+      <div class="buy"> $ {crypto.current_price} </div>
+      <div class="sell">$ {crypto.low_24h}</div>
+    </div>
+    <div class="crypto-prices">
+        <div class="buy">$ {crypto.current_price} </div>
+        <div class="sell">$ {crypto.low_24h}</div>
+    </div>
+    <div class="crypto-prices">
+        <div class="buy"> $ {crypto.current_price} </div>
+        <div class="sell">$ {crypto.low_24h}</div>
+    </div>
+</div>
+
+<style scoped>
+
+.crypto-wrapper {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-center;
+    margin: 5px;
+    width: 100%;
+    background: #2b3389;
+    color: white;
+    padding: 20px 20px;
+    border-radius: 9px;
+}
+
+  .crypto-wrapper > h1 {
+      font-size: 0.9rem;
+  }
+  
+  .crypto-wrapper > h1 > img {
+    position: relative;
+    height: 50px;
+    width: 50px;
+  }
+  
+  .crypto-wrapper .crypto-prices {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 80%;
+    height: 40px;
+    font-size: 0.5rem;
+  }
+  
+  .crypto-prices .buy {
+    border-radius: 6px;
+    background: green;
+  }
+  
+  .crypto-prices .sell {
+    border-radius: 6px;
+    background: red;
+  }
+  
+</style>
