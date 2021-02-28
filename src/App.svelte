60928929e79cb63408381cc47701b0f3985e53b3
unknown <knightkrypto7@gmail.com>
2021-02-28 17:42:15 +0800

    fixed bug and components almost complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,39 @@
+
+<script>
+  import {onMount, tick} from "svelte"
+  import Crypto from "./Crypto.svelte"
+  import axios from "axios"
+
+  $: data = ""
+  $: currency = "usd"
+  $: currency_symbol = currency === "usd" ? "$" : "cad" ? "C$" : "^";
+
+  async function getData(){
+    await tick()
+    const res = await axios(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}`)
+    data = await res.data
+  }
+ 
+  onMount(getData)
+
+</script>
+<div class="content">
+  <div class="head_container">
+  <h1>Cryptocurrency</h1>
+  <select on:change={getData} class="currency_type" bind:value="{currency}">
+    <option value="usd">USD</option>
+    <option value="cad">CAD</option>
+  </select>
+</div>
+  
+  <div class="cryptos">
+      {#each data as crypto} 
+        <Crypto {currency_symbol} {crypto} />
+      {/each}
+  </div>
+  
+</div>
+
 <style>
   :root {
     font-size: 32px;
@@ -28,39 +64,36 @@
    width: 100%;
    max-width: 1000px;
  } 
+
+ .content > .head_container {
+   grid-column: 2/3;
+   display: flex;
+   flex-direction: row;
+   justify-content: space-evenly;
+   align-items: center;
+   background: #2b3389;
+   width: 100%;
+ }
  
-  .content > h1 {
+  .content > .head_container h1 {
     justify-self: center;
     align-self: center;
     font-size: 1.4rem;
     grid-column: 2/3;
     grid-row: 1/2;
+    color: white;
+  }
+
+  .content > .head_container .currency_type {
+    font-size: 0.5rem;
+    border: none;
+    outline: none;
+    color: #ffffff;
+    background: none;
+  }
+
+  .content > .head_container .currency_type option {
+    color: #000000;
   }
   
 </style>
-
-<script>
-  import {onMount} from "svelte"
-  import Crypto from "./Crypto.svelte"
-  import axios from "axios"
-
-  $: data = ""
-  $: currency = "usd"
- 
-  onMount(async function(){
-    const res = await axios(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}`)
-    data = await res.data
-  })
-</script>
-<div class="content">
-  <h1>Cryptocurrency</h1>
-  <select bind:value="{currency}">
-    <option value="usd">USD</option>
-    <option value="cad">CAD</option>
-  </select>
-  <div class="cryptos">
-      {#each data as crypto} 
-        <Crypto {crypto} />
-      {/each}
-  </div>
-</div>
--- src/Crypto.svelte
+++ src/Crypto.svelte
@@ -1,21 +1,15 @@
 <script>
    export let crypto
-   
+   export let currency_symbol
 </script>
 
 <div class="crypto-wrapper">
     <h1>{crypto.name} <img src={crypto.image} alt={crypto.name} /></h1>
     <div class="crypto-prices">
-      <div class="buy"> $ {crypto.current_price} </div>
-      <div class="sell">$ {crypto.low_24h}</div>
-    </div>
-    <div class="crypto-prices">
-        <div class="buy">$ {crypto.current_price} </div>
-        <div class="sell">$ {crypto.low_24h}</div>
-    </div>
-    <div class="crypto-prices">
-        <div class="buy"> $ {crypto.current_price} </div>
-        <div class="sell">$ {crypto.low_24h}</div>
+      <div class="crypto-prices-content buy"><span>Current price</span> <p>{currency_symbol}{crypto.current_price}</p></div>
+      <div class="crypto-prices-content low24"><span>Low 24 hours</span> <p>{currency_symbol}{crypto.low_24h}</p></div>
+      <div class="crypto-prices-content high24"><span>High 24 hours</span> <p>{currency_symbol}{crypto.high_24h}</p></div>
+      <div class="crypto-prices-content total_supply"><span>Total supply</span> <p>{crypto.total_supply || "Not available"}</p></div>
     </div>
 </div>
 
@@ -24,46 +18,70 @@
 .crypto-wrapper {
     position: relative;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-center;
+    flex-direction: row;
+    justify-content: space-evenly;
     margin: 5px;
     width: 100%;
+    height: 190px;
     background: #2b3389;
     color: white;
     padding: 20px 20px;
     border-radius: 9px;
+    transition: transform 0.3s ease-in-out;
+}
+
+.crypto-wrapper:hover {
+  transform: scale(1.1);
 }
 
   .crypto-wrapper > h1 {
-      font-size: 0.9rem;
+      font-size: 0.6rem;
+      display: flex;
+      flex-direction: row;
+      text-align: center;
+      justify-self: center;
+      align-self: center;
+  }
+
+  .crypto-wrapper > h1::after {
+    content: "";
+    position: relative;
+    background: grey;
+    height: 100%;
+    width: 2px;
+    top: 0;
+    right: -10px;
   }
   
   .crypto-wrapper > h1 > img {
     position: relative;
-    height: 50px;
-    width: 50px;
+    height: 30px;
+    width: 30px;
   }
   
   .crypto-wrapper .crypto-prices {
     position: relative;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    width: 60%;
+    font-size: 0.6rem;
+    margin-left: 20px;
+  }
+  
+  .crypto-prices .buy p, .crypto-prices .crypto-prices-content {
     display: flex;
-    flex-direction: row;
-    align-items: center;
+    flex-direction: column;
     justify-content: space-evenly;
-    width: 80%;
-    height: 40px;
+    align-items: center;
+  }
+
+  .crypto-prices .crypto-prices-content span {
     font-size: 0.5rem;
   }
-  
-  .crypto-prices .buy {
-    border-radius: 6px;
-    background: green;
+
+  .crypto-prices .crypto-prices-content p {
+    font-size: 0.4rem;
+    color: rgb(166 182 183);
   }
-  
-  .crypto-prices .sell {
-    border-radius: 6px;
-    background: red;
-  }
-  
+
 </style>
